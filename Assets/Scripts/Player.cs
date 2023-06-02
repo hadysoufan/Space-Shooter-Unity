1320ebe5826536f63e01cd9e2a21bdcc4dbd3e32
hadysoufan <hadisoufan74@gmail.com>
2023-06-02 21:34:39 +0300

    Adding Player boundaries on X & Y axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,25 +12,36 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-
-        // take the current position = new position (0, 0, 0)
-        transform.position = new Vector3(0, 0, 0);
-        
+        transform.position = new Vector3(0, 0, 0);    
     }
 
     void Update()
     {
+
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
-        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-
         transform.Translate(direction * _speed * Time.deltaTime);
-        
 
+        if(transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if(transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if(transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if(transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
 
     }
 }
