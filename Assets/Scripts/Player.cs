c0491a43f190000f4e06b4c9e054bfeabf1c95af
hadysoufan <hadisoufan74@gmail.com>
2023-06-05 21:03:17 +0300

    Adding FireLazer method

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,11 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
     public float horizontalInput, verticalInput;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
 
 
     void Start()
@@ -19,6 +24,13 @@ public class Player : MonoBehaviour
     {
 
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLazer()
+        }
+
+        
 
     }
 
@@ -41,5 +53,11 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11, transform.position.y, 0);
         }
+    }
+
+    void FireLazer()
+    {
+            _canFire = Time.time + _fireRate;
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);  
     }
 }
