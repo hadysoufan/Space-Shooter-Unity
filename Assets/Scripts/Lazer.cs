24b7b90880c539eace8e4b0688f18b1aa0ae9444
hadysoufan <hadisoufan74@gmail.com>
2023-06-05 21:03:53 +0300

    Indicating the position of the lazer

---
+++ Assets/Scripts/Lazer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazer : MonoBehaviour
+{
+
+    private float _speed = 8.0f;
+
+
+    void Update()
+    {
+        
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y > 8f)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
