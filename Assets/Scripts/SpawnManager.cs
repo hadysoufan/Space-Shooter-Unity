a3a25b0535d90405f260c7859002265d22e242ee
hadysoufan <hadisoufan74@gmail.com>
2023-06-09 21:57:40 +0300

    Creating SpawnManager Script

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    private bool _stopSpawing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while(_stopSpawing == false)
+        {
+        
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy =  Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+
+            yield return new WaitForSeconds(5.0f);
+
+        }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawing = true;
+    }
+}
