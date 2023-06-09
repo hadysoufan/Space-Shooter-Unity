6733f14bc7d89ea21c361989d97a967014237205
hadysoufan <hadisoufan74@gmail.com>
2023-06-09 21:56:28 +0300

    Adding Damage Logic of the Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,13 +11,22 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameObject _laserPrefab;
     [SerializeField]
-    private float _fireRate = 0.5f;
+    private float _fireRate = 0.15f;
     private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
+    private SpawnManager _spawnManager;
 
 
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);    
+        transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if (_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is null");
+        }
     }
 
     void Update()
@@ -27,7 +36,7 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
-            FireLazer()
+            FireLazer();
         }
 
         
@@ -59,5 +68,16 @@ public class Player : MonoBehaviour
     {
             _canFire = Time.time + _fireRate;
             Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);  
+    }
+
+    public void Damage()
+    {
+        _lives--;
+
+        if(_lives < 1)
+        {
+            _spawnManager.OnPlayerDeath();
+            Destroy(this.gameObject);
+        }
     }
 }
