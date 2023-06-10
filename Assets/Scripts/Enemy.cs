3d1af941d55e5cce55eba1e99c91c65cd670f12e
hadysoufan <hadisoufan74@gmail.com>
2023-06-10 22:16:34 +0300

    Adding 2D Assets

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -29,7 +29,7 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Hit: " + other.transform.name);
 
