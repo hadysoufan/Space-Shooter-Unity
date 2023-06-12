e0a44be5f52c422ee747539fd31ae1d7922dddec
hadysoufan <hadisoufan74@gmail.com>
2023-06-13 00:17:54 +0300

    Creating UI Script to display score and lives remaining image array list

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text _scoreText;
+    [SerializeField]
+    private Image _LivesImage;
+    [SerializeField]
+    private Sprite[] _liveSprites;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _scoreText.text = "Score: " + 0;
+    }
+
+    public void UpdateScore(int playerScore){
+        _scoreText.text = "Score: " + playerScore.ToString();
+    }
+
+    public void UpdateLives(int currentLives){
+        _LivesImage.sprite = _liveSprites[currentLives];
+    }
+}
